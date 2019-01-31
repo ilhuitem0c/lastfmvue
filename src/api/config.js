2f4962c2be6a0d336f0bb7f8bc8a3b11a809e15d
ilhuitem0c <jc11111001001@gmail.com>
2019-01-31 17:34:50 -0600

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appname: 'Prueblaplatzi',
+  apiKey: 'f6db5cf9045444bc3bccddc34671868d',
+  secret: '9fa1459968fcdaac951981dc066ef15f',
+  registeredTo: 'ilhuitemoc010'
+}
+
+
+//Application name	Prueblaplatzi
+//API key	f6db5cf9045444bc3bccddc34671868d
+//Shared secret	9fa1459968fcdaac951981dc066ef15f
+//Registered to	ilhuitemoc010
